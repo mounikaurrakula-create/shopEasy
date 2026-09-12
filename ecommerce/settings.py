82c739f4cd3a3a2a8ecd282678cd35ec0a19b480
mounikaurrakula-create <mounikaurrakula@gmail.com>
2026-09-12 19:16:52 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -164,8 +164,8 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'PREFIX': '',
 }
-
 
 # ============================================================
 # FILE STORAGE
